8edcddd4709087b58c4c6a7444667a1808233348
Luciano <saenzluciano@gmail.com>
2023-06-27 18:50:29 -0300

    cambios en vista del menu

--- pages/utils.js
+++ pages/utils.js
@@ -1,5 +1,5 @@
 function createPage(title, content){
-    let html ='<!DOCTYPE HTML><html><head><meta charset="UTF-8>'
+    let html ='<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8><meta name="viewport" content="width=device-width, initial-scale=1.0">'
     html += `<title>${title}</title></head><body>`
     html += '<a href="/projects">Home</a> | <a href="/projects?section=mobile">Mobile</a> | <a href="/projects?section=landing">LandingPage</a>| <a href="/projects?section=webapp">Web App</a> | <a href="/projects?section=ecommerce">eCommerce</a> | <a href="/projects?section=game">Games</a>'
     html += content;
